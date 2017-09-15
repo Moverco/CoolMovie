5c1565325fba3338a67778e53372de83eda68609
Moverco <moverco9@gmail.com>
2017-09-15 15:04:43 +0800

    P2.0E
    Add build config

--- app/src/main/java/top/moverco/coolmovie/util/LoggerUtil.java
+++ app/src/main/java/top/moverco/coolmovie/util/LoggerUtil.java
@@ -2,6 +2,8 @@ package top.moverco.coolmovie.util;
 
 import android.util.Log;
 
+import top.moverco.coolmovie.BuildConfig;
+
 /**
  * Created by liuzongxiang on 11/05/2017.
  */
@@ -9,6 +11,7 @@ import android.util.Log;
 public class LoggerUtil {
     public static String TAG = "Cool Movie";
     public static void debug(String msg){
+        if (BuildConfig.OPEN_LOG)
         Log.d(TAG,msg);
     }
 }
